45e8d75b2db074a17f620d4935751c00d5b054cb
Daniil Sherer <dvsherer@mail.ru>
2022-12-23 15:22:15 +0500

    change index.py

--- index.py
+++ index.py
@@ -9,29 +9,30 @@ connection_data = {
 
 
 st.title('Multilingual-finedtuned-ru ')
-hint_text = "**Output**"
+hint_text = "**Вывод**"
 error_text = '**Нужно загрузить модель, для этого [перейдите на страницу модели](https://huggingface.co/makarshatilov/transliteration_v1), нажмите Compute, немного подождите пока модель не загрузится, а затем перезагрузите текущую страницу**'
+description_text = "Модель способная находить ответы на вопросы из контекста."
+st.subheader(description_text)
+
+text_question = st.text_input(label='Введите вопрос')
+text_context = st.text_area(label='Введите контекст')
+button = st.button('Получить ответ')
 
 
 model = Model(connection_data)
 
-text_question = st.text_input(label='Input question')
-print(text_question)
 
-
-if text_question:
-    text_context = st.text_area(label='Input context')
-    print(text_context)
-
-    if text_context:
-        print(text_question)
-        print(text_context)
-        response = model.get_answer(text_question, text_context)
-        print(response)
-        if ('error' in response):
-            st.markdown(error_text, unsafe_allow_html=False)
-        else:
-            st.markdown(hint_text)
-            st.write(response['answer'])
+if button and text_question != '' and text_context != '':
+    response = model.get_answer(text_question, text_context)
+    if ('error' in response):
+        st.markdown(error_text, unsafe_allow_html=False)
     else:
         st.markdown(hint_text)
+        st.write(f"""
+                 Оценка: {response['score']}\n
+                 Ответ: {response['answer']}\n
+                 Ссылка для ознакомления с моделью: https://huggingface.co/AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru
+                  """)
+else:
+    st.markdown(hint_text)
+
